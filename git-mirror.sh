69c2b733cd11c26605f371aaefc536267233a35a
Balint Morasz <balint@morasz.hu>
2022-03-05 11:37:39 +0100

    don't remove pull requests when mirroring a single branch

--- git-mirror.sh
+++ git-mirror.sh
@@ -27,10 +27,9 @@ git remote set-url --push origin "$DESTINATION_REPO"
 if [ -z "$SINGLE_BRANCH" ]
 then
   git fetch -p origin
+  # Exclude refs created by GitHub for pull request.
+  git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 fi
-
-# Exclude refs created by GitHub for pull request.
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 
 if [ "$DRY_RUN" = "true" ]
 then
